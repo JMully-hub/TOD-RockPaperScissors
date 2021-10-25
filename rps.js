7407c80e6921df163a1d0169b89b912de2259426
JMully-hub <mulligan321@hotmail.com>
2021-10-25 22:23:32 +0100

    UI version

--- rps.js
+++ rps.js
@@ -1,74 +1,84 @@
 function computerPlay(){
     let playResult = Math.floor(Math.random() * 3);
     if (playResult === 0){
-        return "rock";
+        return "Rock";
     }
     else if (playResult === 1){
-        return "paper";
+        return "Paper";
     }
     else {
-        return "scissors";
+        return "Scissors";
     }
 }
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
+        draws += 1;
         return "It's a draw!";
     }
-    else if (playerSelection === "paper" && computerSelection === "rock"){
+    else if (playerSelection === "Paper" && computerSelection === "Rock"){
+        wins += 1;
         return "You Win! Paper beats Rock!";
     }
-    else if (playerSelection === "paper" && computerSelection === "scissors"){
+    else if (playerSelection === "Paper" && computerSelection === "Scissors"){
+        losses += 1;
         return "You Lose! Scissors beat Paper!";
     }
-    else if (playerSelection === "rock" && computerSelection === "paper"){
+    else if (playerSelection === "Rock" && computerSelection === "Paper"){
+        losses += 1;
         return "You Lose! Paper beats Rock!";
     }
-    else if (playerSelection === "rock" && computerSelection === "scissors"){
+    else if (playerSelection === "Rock" && computerSelection === "Scissors"){
+        wins += 1;
         return "You Win! Rock beats Scissors!";
     }
-    else if (playerSelection === "scissors" && computerSelection === "rock"){
+    else if (playerSelection === "Scissors" && computerSelection === "Rock"){
+        losses += 1;
         return "You Lose! Rock beats Scissors!";
     }
-    else if (playerSelection === "scissors" && computerSelection === "paper"){
+    else if (playerSelection === "Scissors" && computerSelection === "Paper"){
+        wins += 1;
         return "You Win! Scissors beats Paper!";
     }
 }
 
-function game(){
-    let wins = 0;
-    let losses = 0;
-    let draws = 0;
-    for (let i = 1; i < 6; i++){
-        const inputString = prompt("Please type your selection, Rock, Paper or Scissors:");
-        const playerSelection = inputString.toLowerCase();
+let wins = 0;
+let losses = 0;
+let draws = 0;
+const resultsDiv = document.getElementById("results");
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+            const playerSelection = button.value;
+            const computerSelection = computerPlay();
+            if(wins < 5 && losses < 5){
+                resultsDiv.innerText = "\nYour Choice: " + playerSelection + "\n\nComputer: " + computerSelection  + 
+                "\n\nResult: " + playRound(playerSelection, computerSelection) +"\n\n SCORES\nYou: " + wins + "\nComputer: " + losses +
+                "\nDrawn: " + draws;
+            }else{
+                if(wins>losses){
+                    resultsDiv.innerText = "YOU WIN!"
+                }else{
+                    resultsDiv.innerText = "YOU LOST!"
+            }
+            
+        }
+    })
+})
+
+function game(playerSelection){
+    while (wins < 5 || losses < 5){
         const computerSelection = computerPlay();
-        console.log("Round " + i);
-        console.log("Player: " + playerSelection);
-        console.log("Computer: " + computerSelection);
-        let roundResult = playRound(playerSelection, computerSelection);
-        console.log(roundResult);
-        if (roundResult.includes("You Win!")){
-            wins += 1;
-        }
-        else if (roundResult.includes("You Lose!")){
-            losses += 1;
-        }
-        else {
-            draws += 1;
-        }
-    }
-    if (wins === losses){
-        return "It's a draw! You drew with " + wins + " wins to " + losses + " losses and " + draws + " draw(s)";
-    }
-    else if (wins > losses){
-        return "Well done! You won by " + wins + " wins to " + losses + " losses and " + draws + " draw(s)";
-    }
-    else if (losses > wins){
-        return "Bad luck! You lost by " + wins + " wins to " + losses + " losses and " + draws + " draw(s)";
+        const roundResult = playRound(playerSelection, computerSelection);
+        resultsDiv.innerText = roundResult
     }
 }
-console.log(game());
+
+
+
+
+
 
 
 
